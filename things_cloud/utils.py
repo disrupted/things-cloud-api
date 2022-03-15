e1629ab1b967a2d6bf6e9bcf6056db1b72286ede
disrupted <hi@salomonpopp.me>
2022-03-15 16:38:55 +0100

    fix: mypy reported issues

--- things_cloud/utils.py
+++ things_cloud/utils.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import date, datetime, timedelta, timezone
 
-from shortuuid import ShortUUID
+from shortuuid import ShortUUID  # type: ignore
 
 
 class Util:
