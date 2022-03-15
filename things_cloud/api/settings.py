1be854131003855dce3adb6275dd51305a9233c6
disrupted <hi@salomonpopp.me>
2022-03-15 17:10:59 +0100

    fix: set empty fallback user agent

--- things_cloud/api/settings.py
+++ things_cloud/api/settings.py
@@ -1,4 +1,4 @@
 import os
 
-USER_AGENT = os.environ.get("THINGS_USER_AGENT")
+USER_AGENT = os.environ.get("THINGS_USER_AGENT", "")
 APP_ID = os.environ.get("THINGS_APP_ID", "com.culturedcode.ThingsMac")
