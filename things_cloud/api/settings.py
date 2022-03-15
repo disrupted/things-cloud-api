596faea9acbfecdefb33d63f6ef015a05694ab7b
disrupted <hi@salomonpopp.me>
2022-03-15 17:08:13 +0100

    fix: remove THINGS_ACCOUNT from settings

--- things_cloud/api/settings.py
+++ things_cloud/api/settings.py
@@ -1,5 +1,4 @@
 import os
 
-ACCOUNT = os.environ["THINGS_ACCOUNT"]
-USER_AGENT = os.environ["THINGS_USER_AGENT"]
+USER_AGENT = os.environ.get("THINGS_USER_AGENT")
 APP_ID = os.environ.get("THINGS_APP_ID", "com.culturedcode.ThingsMac")
